9512f14ad21c0476100f183fa54fa3ec49d13295
brandon6190 <brandon6190@outlook.com>
2019-11-15 15:09:02 -0500

    Created a scripts.js file -Created 2 functions OpenSlideMenu() and closeSlideMenu() that hanldes the navbar open and close logic

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function openSlideMenu() {
+  document.getElementById('slide-menu').style.width = '300px';
+  document.getElementById('slide-menu').style.borderTopRightRadius = '25px';
+  document.getElementById('slide-menu').style.borderBottomRightRadius = '25px';
+  // document.getElementById('root').style.marginLeft = '250px';
+}
+
+function closeSlideMenu() {
+  document.getElementById('slide-menu').style.width = '0';
+  // document.getElementById('root').style.marginLeft = '0';
+}
